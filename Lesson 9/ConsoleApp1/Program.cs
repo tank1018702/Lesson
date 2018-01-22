2ad07c37c7d646ff1d9430f95600cc743be4b923
tank1018702 <tank1018702@sina.com>
2018-01-22 18:45:14 +0800

    2018/1/22 in school

---
+++ Lesson 9/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = '↑';
+            int n;
+            n = (int)a;
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
--- Lesson 9/text/Program.cs
+++ Lesson 9/text/Program.cs
@@ -11,13 +11,13 @@ namespace text
         static void Main(string[] args)
         {
             int n = 1;
-            int z = 7;
-            double x = z / 1000.000000 + 1;
+
+            double x = 4.65;
             for (int i = 1; i < 21; i++)
             {
 
-                double d = n / 10.0 + 1;
-                double a =(int)( Math.Log(d, x));
+                double d = n/10.0+1 ;
+                double a =(int)( Math.Pow(x,d));
                 Console.WriteLine("{0}:{1}", i, a);
                 n++;
             }
