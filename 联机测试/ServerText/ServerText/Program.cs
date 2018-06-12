1c82329e0ed5fd03ebd7446d8be3efe6779a83ee
wild hunt <tank1018702@sina.com>
2018-06-12 22:48:31 +0800

    2018/6/12

---
+++ 联机测试/ServerText/ServerText/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerText
+{
+    class Program
+    {
+        private static Socket ServerSocket;
+
+
+
+        static void Receive()
+        {
+            ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress iPAddress = IPAddress.Parse("192.168.2.138");
+            IPEndPoint iPEndPoint=
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
